32d00291af676d77d16d63f3902693685d833912
Dawid Fourie <dawief4@gmail.com>
2023-07-25 17:54:18 -0700

    deployment ready 1

---
+++ Client/src/components/Login.jsx
@@ -0,0 +1,80 @@
+/* eslint react/prop-types: 0 */
+import { useState } from "react"
+import axios from "axios"
+import Register from "./Register"
+
+
+const Login = ({ handleLogin }) => {
+    
+
+    const [isLoggingIn, setIsLoggingIn] = useState(true)
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+    
+
+    const handleFormChange = (e) => {
+        const { name, value } = e.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const { username, password } = formData
+
+            const response = await axios.post(`/users/login`, {
+                user: {
+                    username: username,
+                    password: password
+                }
+            })
+
+            const { token } = response.data
+
+            localStorage.setItem('token', token)
+
+            handleLogin()
+        }
+        catch(error) {
+            console.error('Error during login:', error)
+        }
+        
+    }
+
+    const handleRegistration = () => {
+        setIsLoggingIn((prev) => !prev)
+    }
+    return (
+        <div>
+            {isLoggingIn ? 
+                <form className="form" onSubmit={handleSubmit}>
+                <div className="header">Sign In</div>
+                <div className="inputs">
+                    <input placeholder="Email" className="input" type="text" name="username" autoComplete='username' onChange={handleFormChange} />
+                    <input placeholder="Password" className="input" type="password" name="password" onChange={handleFormChange} />
+                    <div className="checkbox-container">
+                        <label className="checkbox">
+                        <input type="checkbox" id="checkbox" />
+                        </label>
+                        <label htmlFor="checkbox" className="checkbox-text">Remember me</label>
+                    </div>
+                    <button className="sigin-btn" type="submit">Submit</button>
+                    <a className="forget" href="#">Forget password ?</a>
+                    <p className="signup-link">{"Don't have an account? "}<a href="#" onClick={handleRegistration}>Sign up</a></p>
+                </div>
+            </form> :
+            <Register handleRegister={handleRegistration}/>
+            }
+        </div>
+        
+    )
+}
+
+export default Login
---
+++ Client/src/components/Register.jsx
@@ -0,0 +1,70 @@
+/* eslint react/prop-types: 0 */
+import { useState } from 'react'
+import axios from 'axios'
+
+const Register = ({ handleRegister }) => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        password2: ''
+    })
+    
+    const handleFormChange = (e) => {
+        const { name, value } = e.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const { username, password, password2 } = formData
+
+            if (username.trim() === '' || password.trim() === '') {
+                alert('Please fill in all required fields')
+                return
+            }
+
+            if(password !== password2) {
+                alert('Passwords do not match')
+                return
+            }
+
+            const response = await axios.post(`/users/register`, {
+                user: {
+                    username: username,
+                    password: password
+                }
+            })
+            console.log(response.data.message)
+
+            alert('Registration successful! Please log in.')
+            handleRegister()
+        }
+        catch (error) {
+            console.error('Error during registration:', error)
+        }
+        
+    }
+
+    return (
+        <div>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="header">Register</div>
+                <div className="inputs">
+                    <input placeholder="Email" className="input" type="text" name='username' autoComplete='new-username' value={formData.username} onChange={handleFormChange} />
+                    <input placeholder="Password" className="input" type="password" name='password' value={formData.password} onChange={handleFormChange} />
+                    <input placeholder="Re-Enter Password" className="input" type="password" name='password2' value={formData.password2} onChange={handleFormChange} />
+                    <button className="sigin-btn" type="submit">Register</button>
+                </div>
+            </form>
+            <button onClick={() => handleRegister()}>Back</button>
+        </div>
+    )
+}
+
+export default Register
